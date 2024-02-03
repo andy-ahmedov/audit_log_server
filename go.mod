dc410d12ccc021189f45953c7d79d37ada4d3065
Andy <andy.ahmedov2@gmail.com>
2024-02-03 22:38:24 +0300

    Implement Config package

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,41 @@
 package config
 
+import "github.com/kelseyhightower/envconfig"
+
 // создаем структуру Монго в котором будут 4 поля: uri username пароль и database
 // создаем структуру Сервер с единственным полем порт
 // создаем структуру конфиг с двумя структурами выше
-// 
-// создаем функцию New() 
+//
+// создаем функцию New()
 // инициализируем структуру конфиг с помощью функции new
 // используем библу енвконфиг и метод процесс для связывания данных из енв со структурами
 // возвращаем структуру
+
+type Mongo struct {
+	Uri      string
+	Username string
+	Password string
+	Database string
+}
+
+type Server struct {
+	Port int
+}
+
+type Config struct {
+	Db     Mongo
+	Server Server
+}
+
+func New() (*Config, error) {
+	cfg := new(Config)
+
+	if err := envconfig.Process("db", &cfg.Db); err != nil {
+		return nil, err
+	}
+	if err := envconfig.Process("server", &cfg.Server); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
