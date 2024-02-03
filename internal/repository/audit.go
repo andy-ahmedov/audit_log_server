8278cf3828e00f1270049a848ac7e76ebd595582
Andy <andy.ahmedov2@gmail.com>
2024-02-03 22:46:58 +0300

    Implement the Repository package

--- internal/repository/audit.go
+++ internal/repository/audit.go
@@ -1,7 +1,30 @@
-package mongo
+package repository
+
+import (
+	"context"
+
+	audit "github.com/andy-ahmedov/audit_log_server/pkg/domain"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 // создаем структуру аудит с единственным полем бд типа *mongo.Database
 // создаем функцию NewAudit, которая возвращает эту структуру
 // создаем метод Insert, принимающий контекст и audit.LogItem
 // который вставляет второй параметр в Коллекцию логов с помощью дополнительного метода InsertOne, в который и передается контекст с самим логоm
 // возвращаем ошибку
+
+type Audit struct {
+	db *mongo.Database
+}
+
+func NewAudit(db *mongo.Database) *Audit {
+	return &Audit{
+		db: db,
+	}
+}
+
+func (a *Audit) Insert(ctx context.Context, item audit.LogItem) error {
+	_, err := a.db.Collection("log").InsertOne(ctx, item)
+
+	return err
+}
